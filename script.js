18308f156c4d0bb763bbe23954214aeb665c474e
Cory <co.ry@icloud.com>
2021-12-24 11:39:43 -0500

    alerts and return value of playGame func

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
-
 function computerSelect() {
     return computerChoice = choices[Math.floor(Math.random() * choices.length)]
 }
@@ -18,13 +17,17 @@ function playRound() {
     if ( playerChoice == computerChoice) {
         alert("tie")
     }
-    else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "paper") || (playerChoice == "paper" && computerChoice == "rock")) {
-        alert("you win")
-        playerScore++
+    else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "paper") || 
+            (playerChoice == "paper" && computerChoice == "rock")) {
+                playerScore++
+                alert(`You Played: ${playerChoice} They Played: ${computerChoice}!!
+                Great Job!!! Player Score: ${playerScore} Computer Score: ${computerScore} `);
     }  
-    else if ((playerChoice == "scissors" &&computerChoice == "rock") || (playerChoice == "paper" &&computerChoice == "scissors")|| (playerChoice == "rock" &&computerChoice == "paper")) {
-        alert ("you lose")
-        computerScore++
+    else if ((playerChoice == "scissors" &&computerChoice == "rock") || (playerChoice == "paper" &&computerChoice == "scissors")|| 
+            (playerChoice == "rock" &&computerChoice == "paper")) {
+                computerScore++
+                alert(`You Played: ${playerChoice} They Played: ${computerChoice}!!
+                You Lose!!! Player Score: ${playerScore} Computer Score: ${computerScore} `);
     }
     else {
         alert ("not and option, dude!")
@@ -37,20 +40,19 @@ function playGame(){
         computerSelect();
         playRound();
     }
-    if (playerScore < computerScore) {
-        alert("Beaten!!!")
+    if (playerScore > computerScore) {
+        alert(`Beaten!!! Player Score: ${playerScore} Computer Score: ${computerScore} `);
+        return "Winner"
     }
-    else if (computerScore < playerScore) {
-        alert("victorious!!!")
+    else if (computerScore > playerScore) {
+        alert(`Victorious!!! Player Score: ${playerScore} Computer Score: ${computerScore} `);
+        return "Loser"
     }
 }
-
 // console.log(playSelect())
 // console.log(playRound(playSelect(), computerSelect()))
 console.log(playGame())
-console.log(playerScore)
-console.log(computerScore)
-console.log(playerChoice)
-console.log(computerChoice)
-
-
+// console.log(playerScore)
+// console.log(computerScore)
+// console.log(playerChoice)
+// console.log(computerChoice)
