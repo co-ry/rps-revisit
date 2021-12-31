d21b5948e4591b0397985de30dd0e8b3b6bce423
Cory <co.ry@icloud.com>
2021-12-30 21:29:53 -0500

    full game rounds function. working score reset issue

--- script.js
+++ script.js
@@ -3,25 +3,31 @@
 const choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
+let computerChoice
+let playerChoice
+const btns = document.querySelectorAll("button");
+
+zeroScore = playerScore = 0, computerScore = 0;
+
 
 function computerSelect() {
     return computerChoice = choices[Math.floor(Math.random() * choices.length)]
 }
 
-function playRound() {
+function playRound(playerChoice, computerChoice) {
     if ( playerChoice == computerChoice) {
-        alert(`Tie!!\nBoth Played: ${playerChoice.toUpperCase()}\nPlayer Score: ${playerScore} Computer Score: ${computerScore}`)
+        alert(`Tie!!\nBoth Played: ${playerChoice}\nPlayer Score: ${playerScore} Computer Score: ${computerScore}`)
     }
     else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "paper") || 
             (playerChoice == "paper" && computerChoice == "rock")) {
                 playerScore++
-                alert(`You Played: ${playerChoice.toUpperCase()}\nThey Played: ${computerChoice.toUpperCase()}
+                alert(`You Played: ${playerChoice}\nThey Played: ${computerChoice}
                         \nGreat Job!!! Player Score: ${playerScore} Computer Score: ${computerScore} `);
     }  
     else if ((playerChoice == "scissors" &&computerChoice == "rock") || (playerChoice == "paper" &&computerChoice == "scissors")|| 
             (playerChoice == "rock" &&computerChoice == "paper")) {
                 computerScore++
-                alert(`You Played: ${playerChoice.toUpperCase()}\nThey Played: ${computerChoice.toUpperCase()}
+                alert(`You Played: ${playerChoice}\nThey Played: ${computerChoice}
                         \nYou Lose!!! Player Score: ${playerScore} Computer Score: ${computerScore} `);
     }
     else {
@@ -29,29 +35,31 @@ function playRound() {
     }
 }
 
-function playGame(){
-    while (playerScore < 3 && computerScore < 3) {
-        playSelect();
-        computerSelect();
-        playRound();
+
+btns.forEach((button)=>{button.addEventListener('click',()=>{
+    playerChoice = button.id;
+    computerSelect();
+    if  (playerScore < 3 && computerScore < 3) {
+        playRound(playerChoice,computerChoice);
+        return;
     }
+    else {
+        zeroScore;
+        return;
+    }
+
     if (playerScore > computerScore) {
         alert(`Victory is YOURS!!!\nPlayer Score: ${playerScore} Computer Score: ${computerScore} `);
+        zeroScore;
         return "Winner"
     }
     else if (computerScore > playerScore) {
         alert(`You were beaten by Superior Performance!!!\nPlayer Score: ${playerScore} Computer Score: ${computerScore} `);
+        zeroScore;
         return "Loser"
     }
-}
-let computerChoice
-let playerChoice
-const btns = document.querySelectorAll("button");
 
-btns.forEach((button)=>{button.addEventListener('click',()=>{
-    playerChoice = button.id;
-    
-    playRound(playerChoice,computerSelect());
+    // playGame(playRound(playerChoice,computerChoice))
     console.log(playerChoice);
     console.log(computerChoice);
 })});
