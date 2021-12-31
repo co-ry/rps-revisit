58cd55012f54592f934671f2269beb3701d3b054
Cory <co.ry@icloud.com>
2021-12-31 12:51:16 -0500

    game plays in console. buttons added for additional choices

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rps-revisit
 another attempt at the noble art of Rock/Paper/Scissors. the sweet science....
+
+In the wild version im going to attempt to create the finished product that i have in my head 
--- prac.js
+++ prac.js
@@ -1,27 +1,62 @@
 
-// console.log(btns)
+const options = ["paper", "rock", "lizard", "spock", "scissors"];
+const result = [" ties ", " beats ", " loses to "];
 
+let playerScore = 0;
+let computerScore = 0;
 
-// for(i = 0; i < btns.length; i++){
-//         btns[i].addEventListener("click", function playerChoice (e){
-//         });
-//         let playerChoice = e.target.id;
-//         console.log(playerChoice);
-        
-//     } 
-let playerChoice
-const btns = document.querySelectorAll("button");
-
-btns.forEach((button)=>{button.addEventListener('click',()=>{
-    playerChoice = button.id;
-    game(playerChoice);
-})});
-
-function game(){
-    console.log(playerChoice);
-
+function zeroScore() {
+    playerScore = 0;
+    computerScore = 0;
 }
-    
 
-    
-    
+
+
+let choiceComputer;
+let choicePlayer;
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 5);
+    return choiceComputer = options[randomNum];
+} 
+
+
+playRound = function(choicePlayer, choiceComputer) {
+    if (choicePlayer === undefined){
+        return
+    }
+     indexPlayer = options.indexOf(choicePlayer), //spock => 3
+     indexComputer = options.indexOf(choiceComputer), //rock=> 1
+    dif = indexComputer - indexPlayer; // 1 - 3 => -2
+
+    if(dif < 0) { // -2 < 0 => truthy
+        dif += options.length; // -2 + 5 => 3
+    }
+    while(dif > 2) { //3 > 2 => truthy
+        dif -= 2; // 3 - 2 => 1
+    }
+    console.log(choicePlayer + result[dif] + choiceComputer)
+    return choicePlayer + result[dif] + choiceComputer;//spock beats rock
+};
+
+const btns = document.querySelectorAll("button");
+btns.forEach((button)=>{button.addEventListener('click',()=>{
+    choicePlayer = button.id
+    if  (playerScore < 3 && computerScore < 3) {
+        getComputerChoice();
+        playRound(choicePlayer,choiceComputer);
+        return;
+    }
+    else {
+        zeroScore;
+        return;
+    }
+
+    console.log(bigBang(choicePlayer, choiceComputer));
+
+});
+});
+
+
+console.log(getComputerChoice());  // spock losses to paper 
+
