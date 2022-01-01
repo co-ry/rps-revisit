8bbcd6a389ca6eb9f722d907c53436941f73f2ee
Cory <co.ry@icloud.com>
2022-01-01 14:49:04 -0500

    refresh button added and game score stays correct

--- prac.js
+++ prac.js
@@ -1,19 +1,30 @@
 
 const options = ["paper", "rock", "lizard", "spock", "scissors"];
-const result = [" ties ", " beats ", " loses to "];
+const result = [" tied ", " defeated ", " were vanquished by "];
+
+let choiceComputer;
+let choicePlayer;
 
 let playerScore = 0;
 let computerScore = 0;
 
-function zeroScore() {
-    playerScore = 0;
-    computerScore = 0;
+function compareScore(difference){
+    if (difference === 1 && playerScore < 3 && computerScore < 3){
+        playerScore++
+        scores()
+    }
+    else if(difference === 2 && playerScore < 3 && computerScore < 3){
+        computerScore++
+        scores()
+    }
+    else {
+        alert(`You: ${playerScore}\n Computer:${computerScore}`)
+        scores()
+        newGame.style.backgroundColor = 'blue'
+    }
 }
 
 
-
-let choiceComputer;
-let choicePlayer;
 
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 5);
@@ -25,38 +36,68 @@ playRound = function(choicePlayer, choiceComputer) {
     if (choicePlayer === undefined){
         return
     }
-     indexPlayer = options.indexOf(choicePlayer), //spock => 3
-     indexComputer = options.indexOf(choiceComputer), //rock=> 1
-    dif = indexComputer - indexPlayer; // 1 - 3 => -2
+     indexPlayer = options.indexOf(choicePlayer), //rock = 1
+     indexComputer = options.indexOf(choiceComputer), //paper = 0
+    difference = indexComputer - indexPlayer; // 0 - 1 = -1
 
-    if(dif < 0) { // -2 < 0 => truthy
-        dif += options.length; // -2 + 5 => 3
+    if(difference < 0) { // -1 < 0 = true
+        difference += options.length; // -1 + 5 = 4
     }
-    while(dif > 2) { //3 > 2 => truthy
-        dif -= 2; // 3 - 2 => 1
+    while(difference > 2) { //4 > 2 = true
+        difference -= 2; // 4 - 2 = 2. you lose.
     }
-    console.log(choicePlayer + result[dif] + choiceComputer)
-    return choicePlayer + result[dif] + choiceComputer;//spock beats rock
+    return difference;
 };
 
-const btns = document.querySelectorAll("button");
+// Function to refresh page
+function refreshPage() {
+    window.location.reload(true);
+}
+
+const btns = document.querySelectorAll('.btn');
+const para = document.querySelector('p')
+const showPlayerScore = document.querySelector('#player-score')
+const showComputerScore = document.querySelector('#computer-score')
+const btnWrapper = document.querySelector('#btn-wrapper');
+
+// Creating a button to refresh page
+const newGame = document.getElementById('refresh')
+newGame.textContent = `Play again`;
+newGame.classList.add('refresh');
+newGame.addEventListener('click',()=> {
+    refreshPage();
+});
+
+function scores () {
+    showPlayerScore.textContent = playerScore;
+    showComputerScore.textContent = computerScore;      
+}
+
 btns.forEach((button)=>{button.addEventListener('click',()=>{
     choicePlayer = button.id
-    if  (playerScore < 3 && computerScore < 3) {
-        getComputerChoice();
-        playRound(choicePlayer,choiceComputer);
-        return;
-    }
-    else {
-        zeroScore;
-        return;
-    }
-
-    console.log(bigBang(choicePlayer, choiceComputer));
-
-});
+    getComputerChoice();  
+    godDamn(playerScore, computerScore)
+    return
+    });
 });
 
+function godDamn (playerScore, computerScore){
+    playRound(choicePlayer,choiceComputer);
+    compareScore(difference);
+    console.log(`You played ${choicePlayer} and ${result[difference]} ${choiceComputer}`);  
+    return
 
-console.log(getComputerChoice());  // spock losses to paper 
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
